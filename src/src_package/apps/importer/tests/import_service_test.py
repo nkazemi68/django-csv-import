d276115abd8c067a9db2319f753e421085f25e83
nkazemi68 <it.nkazemi@gmail.com>
2025-11-03 03:29:25 +0330

    test(apps): add some tests for importer app

---
+++ src/src_package/apps/importer/tests/import_service_test.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from ..services.import_csv_service import ImportService, ImportResult
+from ..models import Book, ImportTask
+import io, csv
+
+
+class ImportServiceTest(TestCase):
+    def importer_book_creation_test(self):
+        rows = [{
+            "title": "Test Book",
+            "author": "Navid Kazemi",
+            "isbn": "9780135957059",
+            "publication_year": "2025"
+        }]
+        svc = ImportService()
+        res = svc.import_rows(rows)
+        self.assertEqual(res.created, 1)
+        self.assertEqual(len(res.errors), 0)
+        self.assertTrue(Book.objects.filter(isbn="9780135957059").exists())
+
+
+    def importer_duplicate_isbn_skipping_test(self):
+        Book.objects.create(title="Existing Book", author="Navid Kazemi", isbn="ISBN-123", publication_year=2025)
+        rows = [{
+            "title": "New Book",
+            "author": "Kazemi Navid",
+            "isbn": "ISBN-123",
+            "publication_year": "2024"
+        }]
+        svc = ImportService()
+        res = svc.import_rows(rows)
+        self.assertEqual(res.created, 0)
+        self.assertEqual(res.skipped, 1)
+        self.assertTrue(len(res.errors) >= 1)
